f424ca288b7d942e1edf8aeba99062c294fe9756
Brandon <blam7@student.monash.edu>
2015-07-02 16:00:18 +1000

    Made OSLOM processing code

--- Computation_Module/Computation.m
+++ Computation_Module/Computation.m
@@ -19,12 +19,15 @@ Clauset = run_Clauset(DirList); % Runs the Clauset method
 Clauset_final = process_Clauset(Clauset, numnodes); % Processes the output
 
 %% Gopalan
+% Input is either undirected or directed list, but it will treat them as
+% undirected
+% ------------------------------PARAMETERS---------------------------------
 
 
 
 %% Jerry
 % Input is Adjacency matrix
-% PARAMETERS
+% ------------------------------PARAMETERS---------------------------------
 numIters = 120;
 Threshold = 0.09;
 IsBlind = 1;
@@ -41,7 +44,7 @@ ListeningRule = 'majority';
 
 %% NNMF
 % Input is Adjacency matrix
-% PARAMETERS
+% ------------------------------PARAMETERS---------------------------------
 thresh = [0 0.01 0.1]; % Thresholds, can be multiple thresholds.
 
 NNMF = run_NNMF(Mat, thresh); % Runs the NNMF method
@@ -50,17 +53,21 @@ NNMF = run_NNMF(Mat, thresh); % Runs the NNMF method
 
 %% OSLOM
 % Input is a sparse matrix (undirected)
-% PARAMETERS
+% ------------------------------PARAMETERS---------------------------------
 numIter = 100; % Number of iterations within the algorithm
 Tol = 0.1:0.1:1; % Range of tolerances
-filepath = './Computation/OSLOM/OSLOM2'; % File path from module to OSLOM code
-fileback = '../../../'; % File path back
+filepath = './Modules/Computation_Module/Computation/OSLOM'; % File path from module to OSLOM code
+fileback = '../../../../'; % File path back
 
+% Runs the algorithm for OSLOM
 run_OSLOM(Undir, numIter, Tol, filepath, fileback);
+
+% Processes the textfile outputs
+[OSLOM_final] = process_OSLOM(Tol, numnodes);
 
 %% Shen
 % Input is Adjacency matrix
-% PARAMETERS
+% ------------------------------PARAMETERS---------------------------------
 cSize = 7; % Clique size
 cwThresh = 1.3; % Clique weight threshold
 lThresh = 0; % Threshold on links, 0 because we want to preserve data
--- Computation_Module/PostCompute/process_OSLOM.m
+++ Computation_Module/PostCompute/process_OSLOM.m
@@ -1,7 +1,40 @@
-function [] = process_OSLOM(Tol)
+function [Output] = process_OSLOM(Tol, numnodes)
 % Function that processes the output of the OSLOM method
 
+Output = cell(1, length(Tol)); % Creates the cells for the output
+
 for t = Tol
-    filename = ['OSLOM_tol_' t];
+    % Defines the filename for program to read
+    filename = sprintf('OSLOM_tol_%g.txt', t);
     
+    fid = fopen(filename); % Opens file
+    % Scans the text, creating a list, with 0s where it is the next module
+    A = textscan(fid, '%d', 'CommentStyle', '#', 'Delimiter', {' ', '\n'});
+    A = A{1}; % Sets it as the matrix, gets rid of the cell
+    
+    % Closes and deletes the textfile
+    fclose(fid); 
+    command = ['rm ' filename]; % Creates the command to delete the file
+    system(command); % Deletes
+    
+    NumComms = sum(A==0); % Finds the number of communities
+    
+    CommMat = zeros(numnodes, NumComms); % Creates the matrix for each tolerance
+    
+    counter = 1; % Counter for communities
+    
+    for i = 1:length(A) % For the entire A list
+        if ~A(i) == 0 % If it isn't 0
+            CommMat(A(i), counter) = 1; % Places a 1 in the community matrix
+        else
+            counter = counter + 1; % Else increases the counter
+        end
+    end
+    
+    for i = 1:numnodes
+        % Equally distributes the communities
+        CommMat(i, :) = CommMat(i, :)/sum(CommMat(i, :));
+    end
+    
+    Output{Tol == t} = CommMat; % Sets it as the output
 end

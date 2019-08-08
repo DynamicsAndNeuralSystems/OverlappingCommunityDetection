14987ba8275cfbb0b1173923d4e858f0cfca0c12
97aditi <aditidps07@gmail.com>
2019-08-08 19:59:42 +0530

    Run to generate Fig 3

---
+++ Visualisation_Module/Figures/Fig3.m
@@ -0,0 +1,12 @@
+%load benchmark network data
+cd ../../
+load('Computation_Module/Conversions/networks/network56.dat')
+
+%Run OCDAs on benchmark
+cd ../../
+Computation(network56, {'OSLOM','Jerry','Shen','NNMF','Infomap'}, 1, 'networks/community56')
+
+%Visualise performance
+% cd ../
+% cd Visualisation_Module
+Visualisation({'Benchmark', 'OSLOM', 'Shen', 'NNMF', 'Infomap','SLPA'})

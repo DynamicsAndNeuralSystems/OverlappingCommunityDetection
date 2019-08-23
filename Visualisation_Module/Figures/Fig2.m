6fdf47b9b8b69b98a27371974780bcc73b0c7bb5
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-08-23 16:39:30 +1000

    Fixing Fig. 2

--- Visualisation_Module/Figures/Fig2.m
+++ Visualisation_Module/Figures/Fig2.m
@@ -1,27 +1,36 @@
-% loading processed Right Hemisphere human connectome data 
+% loading processed Right Hemisphere human connectome data
 % Roberts method (0.15) on iFOD
-load('RH.mat')
+load('RH.mat','RH')
 
-%Connectome visualisation
+f = figure('color','w');
+
+% Connectome visualisation
+subplot(2,3,[1,2,4,5]); axis('square')
 Visualisebrain(RH);
 
 
 %Degree vs strength plot
-binRH = RH>0;
+subplot(2,3,3);
+binRH = (RH > 0);
 degree = sum(binRH,1)'+sum(binRH,2)-diag(binRH);
 degree = degree/2;
 strength = sum(RH,1)'+sum(RH,2)-diag(RH);
-strengh=strength/2;
-subplot(2,2,3);
-scatter(degree,strength)
+strength = strength/2;
+plot(degree,strength,'ok','MarkerFaceColor','k','MarkerSize',3)
+xlim([0,max(degree)])
 title('B','FontSize', 15);
-xlabel('Degree')
-ylabel('Strength')
+xlabel('Degree, k')
+ylabel('Strength, s')
+% axis('square')
 
 %Degree Histogram
-subplot(2,2,4);
-hist(degree, 20)
+subplot(2,3,6);
+axis('square')
+histogram(degree,20,'FaceColor',0.5*ones(1,3),'EdgeColor','k')
+xlim([0,max(degree)])
 title('C','FontSize', 15);
-xlabel('Degree')
+xlabel('Degree, k')
 ylabel('Frequency')
+% axis('square')
 
+f.Position = [1000        1002         648         336];
